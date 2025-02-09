56b88743984f82a8bc31f743a88d1c84b76340a9
Satham Ussain <satham.edu@gmail.com>
2025-02-10 00:24:46 +0530

    Search functionality added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { products } from "../assets/assets";
 const ShopContext = createContext();
 
@@ -9,10 +9,16 @@ export const useShopContext = () => {
 const ShopContextProvider = ({ children }) => {
   const currency = "$";
   const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(true);
   const value = {
     currency,
     delivery_fee,
     products,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
